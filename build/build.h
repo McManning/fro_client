f8d372e034a63f9efbe4567e09207088dab52ba7
Chase McManning <cmcmanning@gmail.com>
2012-01-22 14:21:13 -0500

    Fixed missing resource files for Win32 build

---
+++ build/build.h
@@ -0,0 +1,21 @@
+
+#ifndef _BUILD_H
+#define _BUILD_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.5.6.0"
+#define VER_MAJOR	1
+#define VER_MINOR	5
+#define VER_RELEASE	6
+#define VER_BUILD	0
+#define COMPANY_NAME	"Sybolt Studios"
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"fro Visual IRC Client"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright (c) Sybolt Studios 2007-2011"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	""
+
+#endif /*_BUILD_H*/
--- core/Common.h
+++ core/Common.h
@@ -1,23 +1,23 @@
 
 /*
  * Copyright (c) 2011 Chase McManning
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights 
+ * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is 
+ * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
 
@@ -30,7 +30,7 @@
 #include <math.h>
 
 // TODO: Include the vars here somehow without actually referencing fro_private.h!
-#include "../build/fro_private.h"
+#include "../build/build.h"
 
 /*	In case the compiler doesn't define it for us */
 #ifndef WIN32
@@ -235,7 +235,7 @@ void offsetRectByDirection(rect* r, direction dir, sShort distance);
 void offsetRectByAngle(rect* r, sShort theta, sShort distance);
 
 //Explode a string into an vString based on the delimiter. If limit # of children are added, it dumps the rest and exits.
-// if includeBlanks is false, it will ignore blank strings 
+// if includeBlanks is false, it will ignore blank strings
 //		(those that are between two delims. Ex: Delim = *, string is **msg***. if includeBlanks is false, the only thing in result will be msg
 //		otherwise, it'll have blank strings filled around msg)
 bool explode(vString* result, string* str, string delim, bool includeBlanks = false, int limit = 0);
